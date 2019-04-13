30eb93d5e1d517ef942701095857cdabde0c4c10
Clement <clement.noguesasensio@gmail.com>
2019-04-13 19:18:22 +0200

    Added the first version of the changeable GamePlayElements, it's just a test for now because weneed the player to be done to be fully functionnal, but it works as a test

---
+++ Assets/Clement/Scripts/ChangeableGPE.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ChangeableGPE : MonoBehaviour
+{
+    [SerializeField]
+    protected bool TESTbigSmallTEST;
+    protected bool TESTbigsmallBufferTEST;
+
+    [SerializeField]
+    protected GPE m_BigGPE;
+    [SerializeField]
+    protected GPE m_MediumGPE;
+    [SerializeField]
+    protected GPE m_SmallGPE;
+
+    [SerializeField]
+    protected bool m_IsGPELocked;
+    protected GPE m_CurrentGPE;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_CurrentGPE = null;
+
+        if (TESTbigSmallTEST)
+        {
+            m_CurrentGPE = Instantiate<GPE>(m_BigGPE, this.transform.position, Quaternion.identity, this.gameObject.transform);
+        }
+
+        else
+        {
+            m_CurrentGPE = Instantiate<GPE>(m_SmallGPE, this.transform.position, Quaternion.identity, this.gameObject.transform);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (TESTbigsmallBufferTEST != TESTbigSmallTEST && !m_IsGPELocked)
+        {
+            if (TESTbigSmallTEST)
+            {
+                ChangeCurrentTo(m_BigGPE);
+            }
+
+            else
+            {
+                ChangeCurrentTo(m_SmallGPE);
+            }
+        }
+
+        TESTbigsmallBufferTEST = TESTbigSmallTEST;
+    }
+
+    void ChangeCurrentTo(GPE _WhichGPE)
+    {
+        Destroy(m_CurrentGPE.gameObject);
+        m_CurrentGPE = Instantiate(_WhichGPE, this.transform.position, Quaternion.identity, this.gameObject.transform);
+    }
+}
