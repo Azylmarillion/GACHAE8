b6060698b91c572820552c513f9e4bcea5afaec4
AliceCrd <alice.couard@gmail.com>
2019-04-15 00:14:23 +0200

    jhygfds

--- Assets/Clement/Scripts/PlayerCharacter.cs
+++ Assets/Clement/Scripts/PlayerCharacter.cs
@@ -4,14 +4,9 @@ using UnityEngine;
 
 public class PlayerCharacter : MonoBehaviour
 {
-<<<<<<< HEAD
     [SerializeField] private float m_TimeObservedBeforeDying = 2;
     [SerializeField] private GameObject m_StonePrefab = null;
     private Vector3 m_OriginalScale = Vector3.zero;
-=======
-    [SerializeField]  private float m_TimeObservedBeforeDying = 2;
-    [SerializeField]  private GameObject m_DeadBodyPrefab = null;
->>>>>>> master
     private float m_TimeSpentObserved;
     public List<GameObject> m_corpses;
     GameObject m_MeshInstance = null;
@@ -42,7 +37,6 @@ public class PlayerCharacter : MonoBehaviour
 
     private void TurnIntoStone()
     {
-<<<<<<< HEAD
         GameManager.m_nbrCadavre--;
         GameManager.E_Death.Invoke();
         transform.localScale -= new Vector3(0.2f, 0.2f, 0.2f);
@@ -50,11 +44,6 @@ public class PlayerCharacter : MonoBehaviour
         if (m_StonePrefab != null && GameManager.m_nbrCadavre >= 0)
         {
             m_corpses.Add(Instantiate(m_StonePrefab, transform.position, Quaternion.identity));
-=======
-        if (m_DeadBodyPrefab != null)
-        {
-            Instantiate(m_DeadBodyPrefab, transform.position, Quaternion.identity);
->>>>>>> master
         }
         else
         {
--- Assets/Clement/Scripts/PostMortemGPE.cs
+++ Assets/Clement/Scripts/PostMortemGPE.cs
@@ -30,18 +30,9 @@ public class PostMortemGPE : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-<<<<<<< HEAD
         PlayerCharacter player = collision.GetComponent<PlayerCharacter>();
 
         if (player && m_CorpseToReveal)
-=======
-        if (GameManager.m_nbrCadavre >= 0)
-        {
-            Instantiate(m_MeshToReveal, collision.transform.position, new Quaternion(0, 0, 0, 0));
-            m_FailTrigger.enabled = false;
-        }
-        else
->>>>>>> master
         {
             GameManager.m_nbrCadavre--;
             GameManager.E_Death.Invoke();
