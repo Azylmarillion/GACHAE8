bb007906e204563118ecd99546f56ea5c1af6430
AliceCrd <alice.couard@gmail.com>
2019-04-14 18:54:13 +0200

    Reglage animation + animator OK

--- Assets/Alice/Scripts/Menu.cs
+++ Assets/Alice/Scripts/Menu.cs
@@ -1,121 +1,121 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.SceneManagement;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.UI;
+//using UnityEngine.SceneManagement;
 
-public class Menu : MonoBehaviour
-{
-    [SerializeField] private GameObject m_OptionMenu;
-    [SerializeField] private GameObject m_ControlsMenu;
-    [SerializeField] private GameObject m_PauseMenu;
-    [SerializeField] private GameObject m_Credits;
-    [SerializeField] private AudioSource m_audioSourceScene;
+//public class Menu : MonoBehaviour
+//{
+//    [SerializeField] private GameObject m_OptionMenu;
+//    [SerializeField] private GameObject m_ControlsMenu;
+//    [SerializeField] private GameObject m_PauseMenu;
+//    [SerializeField] private GameObject m_Credits;
+//    [SerializeField] private AudioSource m_audioSourceScene;
 
 
-    private void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.A) && SceneManager.GetActiveScene().name != "SceneAlice")
-        {
-            ManagePauseMenu();
-        }
-    }
+//    private void Update()
+//    {
+//        if(Input.GetKeyDown(KeyCode.A) && SceneManager.GetActiveScene().name != "SceneAlice")
+//        {
+//            ManagePauseMenu();
+//        }
+//    }
 
-    public void Play()
-    {
-        SceneManager.LoadScene("Test");
-    }
+//    public void Play()
+//    {
+//        SceneManager.LoadScene("Test");
+//    }
 
-    public void ManagePauseMenu()
-    {
-        if(!m_OptionMenu.activeInHierarchy && !m_ControlsMenu.activeInHierarchy)
-        {
-            m_PauseMenu.SetActive(!m_PauseMenu.activeInHierarchy);
-        }
+//    public void ManagePauseMenu()
+//    {
+//        if(!m_OptionMenu.activeInHierarchy && !m_ControlsMenu.activeInHierarchy)
+//        {
+//            m_PauseMenu.SetActive(!m_PauseMenu.activeInHierarchy);
+//        }
       
-    }
+//    }
 
-    public void Resume()
-    {
-        m_PauseMenu.SetActive(false);
-    }
+//    public void Resume()
+//    {
+//        m_PauseMenu.SetActive(false);
+//    }
 
-    public void ReturnToMenu()
-    {
-        SceneManager.LoadScene("SceneAlice");
-    }
+//    public void ReturnToMenu()
+//    {
+//        SceneManager.LoadScene("SceneAlice");
+//    }
 
-    #region OPTIONS
-    public void OpenOptions()
-    {
-        m_OptionMenu.SetActive(true);
-    }
+//    #region OPTIONS
+//    public void OpenOptions()
+//    {
+//        m_OptionMenu.SetActive(true);
+//    }
 
-    public void SetVolume(float _volume)
-    {
-        m_audioSourceScene.volume = _volume;
-    }
+//    public void SetVolume(float _volume)
+//    {
+//        m_audioSourceScene.volume = _volume;
+//    }
 
-    public void SetQuality(int _qualityIndex)
-    {
-        QualitySettings.SetQualityLevel(_qualityIndex);
-    }
+//    public void SetQuality(int _qualityIndex)
+//    {
+//        QualitySettings.SetQualityLevel(_qualityIndex);
+//    }
 
-    public void ScreenManager(bool _wantFullScreen)
-    {
-        if (_wantFullScreen && !Screen.fullScreen)
-        {
-            Screen.fullScreen = true;
-        }
-        else if (!_wantFullScreen && Screen.fullScreen)
-        {
-            Screen.fullScreen = false;
-        }
+//    public void ScreenManager(bool _wantFullScreen)
+//    {
+//        if (_wantFullScreen && !Screen.fullScreen)
+//        {
+//            Screen.fullScreen = true;
+//        }
+//        else if (!_wantFullScreen && Screen.fullScreen)
+//        {
+//            Screen.fullScreen = false;
+//        }
 
-    }
+//    }
 
-    public void CloseOptions()
-    {
-        m_OptionMenu.SetActive(false);
-    }
-    #endregion
+//    public void CloseOptions()
+//    {
+//        m_OptionMenu.SetActive(false);
+//    }
+//    #endregion
 
-    public void OpenControls()
-    {
-        m_OptionMenu.SetActive(false);
-        m_ControlsMenu.SetActive(true);
-    }
+//    public void OpenControls()
+//    {
+//        m_OptionMenu.SetActive(false);
+//        m_ControlsMenu.SetActive(true);
+//    }
 
-    public void CloseControls()
-    {
-        m_ControlsMenu.SetActive(false);
-        m_OptionMenu.SetActive(true);
-    }
+//    public void CloseControls()
+//    {
+//        m_ControlsMenu.SetActive(false);
+//        m_OptionMenu.SetActive(true);
+//    }
 
-    public void OpenCredits()
-    {
-        m_Credits.SetActive(true);
-    }
+//    public void OpenCredits()
+//    {
+//        m_Credits.SetActive(true);
+//    }
 
-    public void CloseCredits()
-    {
-        m_Credits.SetActive(false);
-    }
-<<<<<<< HEAD
-}
+//    public void CloseCredits()
+//    {
+//        m_Credits.SetActive(false);
+//    }
+//<<<<<<< HEAD
+//}
 
-public void Quit()
-{
-=======
+//public void Quit()
+//{
+//=======
 
-    public void Quit()
-    {
->>>>>>> master
-#if UNITY_EDITOR
+//    public void Quit()
+//    {
+//>>>>>>> master
+//#if UNITY_EDITOR
 
-    UnityEditor.EditorApplication.isPlaying = false;
-#else
-        Application.Quit();
-#endif
-}
-}
+//    UnityEditor.EditorApplication.isPlaying = false;
+//#else
+//        Application.Quit();
+//#endif
+//}
+//}
