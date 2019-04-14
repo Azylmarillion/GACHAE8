f9485ba5d37f1ab19d5f54455876a30c4702fd38
AliceCrd <alice.couard@gmail.com>
2019-04-14 21:09:54 +0200

    GROS PUSH texture + props

---
+++ Assets/Alice/Scripts/ButtonActivatedDoor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonActivatedDoor : MonoBehaviour
+{
+    [SerializeField] private bool m_isActivated_test = false;
+    private List<Button> m_buttons;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
