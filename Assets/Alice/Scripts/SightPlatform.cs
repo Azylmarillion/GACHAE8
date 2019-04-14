a62a0f4b9a2c8bde7acad9ac2ae0471cb4beefe8
AliceCrd <alice.couard@gmail.com>
2019-04-14 09:13:26 +0200

    Add sight platform script + prefabs +delete insults scripts

---
+++ Assets/Alice/Scripts/SightPlatform.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SightPlatform : MonoBehaviour
+{
+    public GameObject m_Player;
+    public float m_Range = 3.0f;
+    private SpriteRenderer m_Mesh;
+
+    void Start()
+    {
+        GameManager.m_SightPower = true;
+        m_Mesh = GetComponent<SpriteRenderer>();
+
+        if (!m_Mesh)
+        {
+            Debug.LogError("Can't find mesh renderer on platform");
+            return;
+        }
+        
+        m_Mesh.enabled = false;
+    }
+
+    void Update()
+    {
+        if (GameManager.m_SightPower)
+        {
+          
+            Vector3 distanceToPlayer = transform.position - m_Player.transform.position;
+            Debug.Log(distanceToPlayer.magnitude);
+            SetMeshVisibility(distanceToPlayer.magnitude <= m_Range);
+        }
+    }
+
+    private void SetMeshVisibility(bool _isVisible)
+    {
+        m_Mesh.enabled = _isVisible;
+    }
+}
