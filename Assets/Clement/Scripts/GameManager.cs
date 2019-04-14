80995df88f98033c0c42ce865c5d172adef72ccf
Clement <clement.noguesasensio@gmail.com>
2019-04-14 03:16:08 +0200

    Added PostMortemGPE and a test map, sorry I didn't do much of the new features but i'm tired so i'm going to sleep

---
+++ Assets/Clement/Scripts/GameManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+static public class GameManager
+{
+    public static UnityEvent E_Death = new UnityEvent();
+
+    public static bool m_JumpSpeedPower = false;
+    public static Vector3 m_RespawnPoint = new Vector3(-8, 2, 0);
+
+}
---
+++ Assets/Clement/Scripts/PostMortemGPE.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PostMortemGPE : MonoBehaviour
+{
+    [SerializeField]
+    GameObject m_MeshToReveal;
+    BoxCollider2D m_FailTrigger;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_FailTrigger = GetComponent<BoxCollider2D>();
+        m_MeshToReveal.SetActive(false);
+        GameManager.E_Death.AddListener(OnPlayerDeath);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameManager.E_Death.Invoke();
+        collision.transform.position = GameManager.m_RespawnPoint;
+
+        m_MeshToReveal.SetActive(true);
+        m_FailTrigger.enabled = false;
+
+    }
+
+    private void OnPlayerDeath()
+    {
+        m_MeshToReveal.SetActive(false);
+        m_FailTrigger.enabled = true;
+    }
+}
