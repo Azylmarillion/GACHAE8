c73503546ac92b906b83172e919877d88408a5de
Azy <william.comminges@gmail.com>
2019-04-14 18:37:31 +0200

    Creation animation complete + animator (SWITCH PC WILL TO ALICE)

--- Assets/Alice/Scripts/Menu.cs
+++ Assets/Alice/Scripts/Menu.cs
@@ -79,13 +79,13 @@ public class Menu : MonoBehaviour
     }
 }
 
-    public void Quit()
-    {
+public void Quit()
+{
 #if UNITY_EDITOR
 
-        UnityEditor.EditorApplication.isPlaying = false;
- #else
+    UnityEditor.EditorApplication.isPlaying = false;
+#else
         Application.Quit();
 #endif
-    }
+}
 }
